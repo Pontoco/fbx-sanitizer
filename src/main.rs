db5f19518bee2fe83c4c8cb57602e47ba5180278
John Austin <kleptine@gmail.com>
2021-03-22 23:37:27 -0600

    Clean and lint.

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,34 @@
-use mint::Vector3;
-use cgmath::{InnerSpace, MetricSpace, Vector2, Zero};
-use fbxcel_dom::fbxcel::tree::v7400::NodeHandle;
-use fbxcel_dom::v7400::object::model::{ModelHandle, TypedModelHandle};
-use fbxcel_dom::v7400::object::{
-    property::loaders::PrimitiveLoader, ObjectHandle, ObjectId, TypedObjectHandle,
-};
-use fbxcel_dom::v7400::Document;
-use fbxcel_dom::{any::AnyDocument, v7400::object::property::PropertiesHandle};
-use std::borrow::Cow;
-use std::collections::hash_map::{RandomState, Values};
-use std::collections::{HashMap, HashSet};
-use std::error::Error;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::Write;
 use std::io::{BufReader, BufWriter};
-use std::iter::FilterMap;
-use std::ops::Deref;
 use std::path::PathBuf;
-use walkdir::WalkDir;
 
 use anyhow::format_err;
+use cgmath::{Zero};
 use fbxcel_dom::fbxcel::low::v7400::AttributeValue;
-use fbxcel_dom::v7400::object::geometry::TypedGeometryHandle;
-
+use fbxcel_dom::fbxcel::tree::v7400::NodeHandle;
+use fbxcel_dom::v7400::object::model::TypedModelHandle;
+use fbxcel_dom::v7400::object::{ObjectId, TypedObjectHandle};
+use fbxcel_dom::v7400::Document;
+use fbxcel_dom::{any::AnyDocument, v7400::object::property::PropertiesHandle};
+use mint::Vector3;
+use walkdir::WalkDir;
 
 fn main() {
     let model_name = "suzanne_283";
-    let mut writer =
-        BufWriter::new(File::create(model_name.to_owned() + ".yml").expect("Failed to open output file"));
+    let mut writer = BufWriter::new(
+        File::create(model_name.to_owned() + ".yml").expect("Failed to open output file"),
+    );
 
     // for file in WalkDir::new(r".")
     // for file in WalkDir::new(r"C:\Projects\FBX_Import_Testing\Assets\".to_owned() + model_name + ".fbx")
-    for file in WalkDir::new(r"C:\Projects\Clockwork\CloningMain\Assets\Game\Environment\Kitchenette\Bowl\Bowl.fbx")
-        .follow_links(true)
-        .into_iter()
-        .filter_map(|e| e.ok())
+    for file in WalkDir::new(
+        r"C:\Projects\Clockwork\CloningMain\Assets\Game\Environment\Kitchenette\Bowl\Bowl.fbx",
+    )
+    .follow_links(true)
+    .into_iter()
+    .filter_map(|e| e.ok())
     {
         let f_name = file.file_name().to_string_lossy().clone();
         let path = file.clone().into_path();
@@ -57,8 +51,8 @@ fn check_fbx_file(path: &PathBuf, writer: &mut BufWriter<File>) -> Result<(), an
     let reader = BufReader::new(file);
 
     match AnyDocument::from_seekable_reader(reader)? {
-        AnyDocument::V7400(fbx_ver, doc) => {
-            print_children(writer, &doc.tree().root(), 0);
+        AnyDocument::V7400(_, doc) => {
+            print_children(writer, &doc.tree().root(), 0)?;
             let mut errors = vec![];
 
             errors.extend(verify_roots_have_identity_transform(&doc)?);
@@ -72,12 +66,12 @@ fn check_fbx_file(path: &PathBuf, writer: &mut BufWriter<File>) -> Result<(), an
             for e in errors {
                 println!("{}", e);
             }
-            print_children(writer, &doc.tree().root(), 5);
+            print_children(writer, &doc.tree().root(), 5)?;
         }
         _ => panic!("Got FBX document of unsupported version"),
     }
 
-    return Ok(());
+    Ok(())
 }
 
 fn verify_roots_have_identity_transform(doc: &Document) -> anyhow::Result<Vec<String>> {
@@ -100,7 +94,7 @@ fn verify_roots_have_identity_transform(doc: &Document) -> anyhow::Result<Vec<St
         // No scale implies a scale of 1
         if let Some(scl) = root.local_scale()? {
             let s: cgmath::Vector3<f64> = scl.into();
-            if !s.eq(&cgmath::Vector3::<f64>::zero()) {
+            if !s.eq(&cgmath::Vector3::<f64>::new(1f64, 1f64, 1f64)) {
                 errors.push(format!(
                     "The root object [{}] does not have a scale of 1. It has scale: [{:?}]",
                     name, s
@@ -109,18 +103,20 @@ fn verify_roots_have_identity_transform(doc: &Document) -> anyhow::Result<Vec<St
         }
     }
 
-    return Ok(errors);
+    Ok(errors)
 }
 
 fn verify_units_are_in_meters(doc: &Document) -> Option<String> {
+    const METERS_SCALE: f64 = 100.0f64;
+
     let settings = match doc.global_settings() {
         Some(s) => s,
         None => return Some("File has no units. (No GlobalSettings)".into()),
     };
 
-    let scale_factor_prop = match settings.get_property("UnitScaleFactor"){
+    let scale_factor_prop = match settings.get_property("UnitScaleFactor") {
         None => return Some("File has no units. (No UnitScaleFactor)".into()),
-        Some(p) => p
+        Some(p) => p,
     };
 
     let scale_factor_value = match scale_factor_prop.value_part().get(0) {
@@ -129,12 +125,15 @@ fn verify_units_are_in_meters(doc: &Document) -> Option<String> {
     };
 
     if let AttributeValue::F64(scale_factor) = scale_factor_value {
-        if *scale_factor != 100.0f64 {
-            return Some(format!("File is not in meter units. Units: {}cm. Should be 100.0cm.", *scale_factor))
+        if *scale_factor != METERS_SCALE {
+            return Some(format!(
+                "File is not in meter units. Units: {}cm. Should be 100.0cm.",
+                *scale_factor
+            ));
         }
     }
 
-    return None;
+    None
 }
 
 // fn verify_bounding_box_size(doc: &Document) -> Option<String> {
@@ -149,6 +148,7 @@ fn verify_units_are_in_meters(doc: &Document) -> Option<String> {
 //     return None;
 // }
 
+#[allow(unused)]
 fn verify_blender_exports_have_correct_axis(doc: &Document) -> Result<Vec<String>, anyhow::Error> {
     let node = doc
         .tree()
@@ -160,7 +160,8 @@ fn verify_blender_exports_have_correct_axis(doc: &Document) -> Result<Vec<String
     let creator = &node.attributes()[0];
     if let AttributeValue::String(c) = creator {
         if c.contains("Blender") {
-            let axis = get_coordinate_axis(doc).ok_or(format_err!("Could not find coordinate axis."))?;
+            let axis = get_coordinate_axis(doc)
+                .ok_or_else(|| format_err!("Could not find coordinate axis."))?;
 
             let correct = CoordinateAxis {
                 up: [0, 0, 1].into(),
@@ -169,20 +170,23 @@ fn verify_blender_exports_have_correct_axis(doc: &Document) -> Result<Vec<String
             };
 
             if axis != correct {
-                return Ok(vec![format!("File has incorrect Blender Coordinate Axis. Expected: [{:?}] actual [{:?}]", correct, axis)]);
+                return Ok(vec![format!(
+                    "File has incorrect Blender Coordinate Axis. Expected: [{:?}] actual [{:?}]",
+                    correct, axis
+                )]);
             }
         }
     }
 
-    return Ok(vec![]);
+    Ok(vec![])
 }
 // could be worth contributing, not sure
 
 #[derive(Debug, PartialEq)]
 struct CoordinateAxis {
-    up: Vector3<bool>,
-    front: Vector3<bool>,
-    coord: Vector3<bool>,
+    up: Vector3<i8>,
+    front: Vector3<i8>,
+    coord: Vector3<i8>,
 }
 
 fn get_coordinate_axis(doc: &Document) -> Option<CoordinateAxis> {
@@ -191,26 +195,24 @@ fn get_coordinate_axis(doc: &Document) -> Option<CoordinateAxis> {
         .ok_or("Count not find global settings in file.")
         .ok()?;
 
-    let upAxis = get_axis(&global_settings, "UpAxis")?;
-    let frontAxis = get_axis(&global_settings, "FrontAxis")?;
-    let coordAxis = get_axis(&global_settings, "CoordAxis")?;
+    let up_axis = get_axis(&global_settings, "UpAxis")?;
+    let front_axis = get_axis(&global_settings, "FrontAxis")?;
+    let coord_axis = get_axis(&global_settings, "CoordAxis")?;
 
     Some(CoordinateAxis {
-        up: upAxis,
-        front: frontAxis,
-        coord: coordAxis,
+        up: up_axis,
+        front: front_axis,
+        coord: coord_axis,
     })
 }
 
-fn get_axis(global_settings: &PropertiesHandle, name: &str) -> Option<Vector3<f64>> {
-    let axis = if let AttributeValue::I32(v) = global_settings
-        .get_property(name)?
-        .value_part()
-        .get(0)? {
-        v
-    } else {
-        return None;
-    };
+fn get_axis(global_settings: &PropertiesHandle, name: &str) -> Option<Vector3<i8>> {
+    let axis =
+        if let AttributeValue::I32(v) = global_settings.get_property(name)?.value_part().get(0)? {
+            v
+        } else {
+            return None;
+        };
 
     let sign = if let AttributeValue::I32(v) = global_settings
         .get_property(&(name.to_owned() + "Sign"))?
@@ -223,34 +225,35 @@ fn get_axis(global_settings: &PropertiesHandle, name: &str) -> Option<Vector3<f6
     };
 
     Some(match axis {
-        0 => [*sign as f64, 0f64, 0f64].into(),
-        1 => [0f64, *sign as f64, 0f64].into(),
-        2 => [0f64, 0f64, *sign as f64].into(),
-        _ => return None
+        0 => [*sign as i8, 0, 0].into(),
+        1 => [0, *sign as i8, 0].into(),
+        2 => [0, 0, *sign as i8].into(),
+        _ => return None,
     })
 }
 
-fn get_models<'a>(doc: &'a Document) -> impl Iterator<Item=TypedModelHandle<'a>> {
+fn get_models(doc: &Document) -> impl Iterator<Item = TypedModelHandle<'_>> {
     return doc.objects().filter_map(|o| {
         if let TypedObjectHandle::Model(model) = o.get_typed() {
             return Some(model);
         }
-        return None;
+        None
     });
 }
 
-fn get_model_roots<'a>(doc: &'a Document) -> Vec<TypedModelHandle<'a>> {
+fn get_model_roots(doc: &Document) -> Vec<TypedModelHandle<'_>> {
     let mut results: HashMap<ObjectId, TypedModelHandle> = HashMap::new();
     for model in get_models(doc) {
         let root = model.root_model();
         results.insert(root.object_id(), root);
     }
-    let vec = results.values().cloned().collect();
-    return vec;
+
+    results.values().cloned().collect()
 }
 
 // end
 
+#[allow(unused)]
 fn print_models(writer: &mut BufWriter<File>, doc: &Document, tabs: i32) {
     for object in doc.objects() {
         if let TypedObjectHandle::Model(model) = object.get_typed() {
@@ -259,21 +262,27 @@ fn print_models(writer: &mut BufWriter<File>, doc: &Document, tabs: i32) {
     }
 }
 
-fn print_children(writer: &mut BufWriter<File>, node: &NodeHandle, tabs: i32) {
+fn print_children(
+    writer: &mut BufWriter<File>,
+    node: &NodeHandle,
+    tabs: i32,
+) -> anyhow::Result<()> {
     write!(
         writer,
         "{:indent$}Name: {} - ",
         "",
         node.name(),
         indent = tabs as usize
-    );
+    )?;
     for attr in node.attributes() {
-        write!(writer, "{:?}  ", attr);
+        write!(writer, "{:?}  ", attr)?;
     }
 
-    write!(writer, "\n");
+    writeln!(writer,)?;
 
     for child in node.children() {
-        print_children(writer, &child, tabs + 2);
+        print_children(writer, &child, tabs + 2)?;
     }
+
+    Ok(())
 }
