6b858b2be47a1dd92c91fc7b41b794c3d5bd1807
John Austin <kleptine@gmail.com>
2021-03-28 22:01:28 -0600

    Adds tests.

---
+++ fbx-sanitizer/tests/integration_tests.rs
@@ -0,0 +1,56 @@
+use assert_cmd::Command;
+use std::path::PathBuf;
+
+#[test]
+fn blender_fbx_passes() {
+    let mut command = Command::cargo_bin("fbx_sanitizer").unwrap();
+
+    let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    d.push("tests/blender_export_good.fbx");
+    command.args(&[d]);
+    command.assert().success();
+}
+
+#[test]
+fn blender_fbx_fails() {
+    let mut command = Command::cargo_bin("fbx_sanitizer").unwrap();
+    let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    d.push("tests/blender_export_bad.fbx");
+    command.args(&[d]);
+    command.assert().failure();
+}
+
+#[test]
+fn maya_fbx_passes() {
+    let mut command = Command::cargo_bin("fbx_sanitizer").unwrap();
+    let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    d.push("tests/maya_export_good.fbx");
+    command.args(&[d]);
+    command.assert().success();
+}
+
+#[test]
+fn maya_fbx_fails() {
+    let mut command = Command::cargo_bin("fbx_sanitizer").unwrap();
+    let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    d.push("tests/maya_export_bad.fbx");
+    command.args(&[d]);
+    command.assert().failure();
+}
+#[test]
+fn max_fbx_passes() {
+    let mut command = Command::cargo_bin("fbx_sanitizer").unwrap();
+    let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    d.push("tests/max_export_good.fbx");
+    command.args(&[d]);
+    command.assert().success();
+}
+
+#[test]
+fn max_fbx_fails() {
+    let mut command = Command::cargo_bin("fbx_sanitizer").unwrap();
+    let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    d.push("tests/maya_export_bad.fbx");
+    command.args(&[d]);
+    command.assert().failure();
+}
